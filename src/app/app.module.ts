8395d4a6c967c51d0d0abbe7dc14587ef71e14fa
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 19:10:03 +0200

    chore: format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './route/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
