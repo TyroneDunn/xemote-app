5b74e6086647251847f6e85c34cd5d53c040bb64
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 20:46:38 +0200

    feat: define inventory page component

---
+++ src/app/inventory/inventory-page/inventory-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-inventory-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './inventory-page.component.html',
+  styleUrls: ['./inventory-page.component.scss']
+})
+export class InventoryPageComponent {
+
+}
