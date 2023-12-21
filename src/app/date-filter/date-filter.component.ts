be14213522b5d570102f13e6ede92b7bc20e7563
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 19:21:06 +0200

    feat: define date filter component

---
+++ src/app/date-filter/date-filter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+   selector: 'app-date-filter',
+   standalone: true,
+   imports: [ CommonModule ],
+   templateUrl: './date-filter.component.html',
+   styleUrls: [ './date-filter.component.scss' ],
+})
+export class DateFilterComponent {
+
+}
