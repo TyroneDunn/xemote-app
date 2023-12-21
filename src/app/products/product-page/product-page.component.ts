daf4e05d4bfb58f1fc3c23469aed3277f1ce0faf
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 20:41:51 +0200

    feat: define product page

---
+++ src/app/products/product-page/product-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.scss']
+})
+export class ProductPageComponent {
+
+}
