bb408a86c9d09cf38a8233852c1799ab50fc663e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 20:44:25 +0200

    feat: define orders page component

---
+++ src/app/orders/orders-page/orders-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+   selector: 'app-orders-page',
+   standalone: true,
+   imports: [ CommonModule ],
+   templateUrl: './orders-page.component.html',
+   styleUrls: [ './orders-page.component.scss' ],
+})
+export class OrdersPageComponent {
+
+}
