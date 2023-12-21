765ab9c58ec1d8fec007402e7546297052e57619
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 21:36:37 +0200

    feat: add auth guard redirect

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,11 +1,23 @@
 import { inject, Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
 import { AuthService } from "./auth.service";
+import { Router } from "@angular/router";
+import { APP_SIGN_IN } from "../../environments/constants";
 
 @Injectable({
    providedIn: 'root',
 })
 export class AuthGuard {
    private authService = inject(AuthService);
-   public canActivate = (): Observable<boolean> => this.authService.authorized$;
+   private router = inject(Router);
+   public canActivate = (): Observable<boolean> => {
+      return this.authService.authorized$
+      .pipe(
+         map(authorized => {
+            if (authorized) return true;
+            else this.router.navigate([ APP_SIGN_IN ]);
+            return false;
+         }),
+      );
+   };
 }
