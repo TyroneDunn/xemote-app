d36221c4964b197517312b7c88f2549a74cf0343
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 19:06:49 +0200

    feat: implement http service

---
+++ src/app/http/http.service.ts
@@ -0,0 +1,41 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { Observable, retry } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+   private readonly http = inject(HttpClient);
+   private readonly options = {
+      observe: 'response' as const,
+      withCredentials: true,
+      headers: new HttpHeaders()
+      .set("Content-Type", "application/json")
+      .set("credentials", "true")
+   };
+
+   public getRequest$<T>(url: string): Observable<HttpResponse<T>> {
+      return this.http.get<T>(url, this.options).pipe(
+         retry(2),
+      );
+   }
+
+   public postRequest$(url: string, payload: object): Observable<HttpResponse<object>> {
+      return this.http.post(url, payload, this.options).pipe(
+         retry(2),
+      );
+   }
+
+   public deleteRequest$(url: string): Observable<HttpResponse<object>> {
+      return this.http.delete(url, this.options).pipe(
+         retry(2),
+      );
+   }
+
+   public patchRequest$<T>(url: string, payload: object): Observable<HttpResponse<T>> {
+      return this.http.patch<T>(url, payload, this.options).pipe(
+         retry(2),
+      );
+   }
+}
