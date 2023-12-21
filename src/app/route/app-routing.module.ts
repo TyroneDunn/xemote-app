ff79951b11fcedd743ceb66ad5a6bd93ef2dc6ab
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 20:40:53 +0200

    feat: define products route

--- src/app/route/app-routing.module.ts
+++ src/app/route/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "../home/home.component";
 import { SignInComponent } from "../sign-in/sign-in.component";
 import { AboutComponent } from "../about/about.component";
+import { ProductsPageComponent } from "../products/products-page/products-page.component";
 
 const routes: Routes = [
    { path: '', component: HomeComponent },
    { path: 'about', component: AboutComponent },
    { path: 'sign-in', component: SignInComponent },
+   { path: 'products', component: ProductsPageComponent },
 ];
 
 @NgModule({
