d1ef450c9d615fca79efd19c3b378d949fc99e8e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 19:25:43 +0200

    chore: format

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-search',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+   selector: 'app-search',
+   standalone: true,
+   imports: [ CommonModule ],
+   templateUrl: './search.component.html',
+   styleUrls: [ './search.component.scss' ],
 })
 export class SearchComponent {
 
--- src/app/toolbar-shadow/toolbar-shadow.component.ts
+++ src/app/toolbar-shadow/toolbar-shadow.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-toolbar-shadow',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './toolbar-shadow.component.html',
-  styleUrls: ['./toolbar-shadow.component.scss']
+   selector: 'app-toolbar-shadow',
+   standalone: true,
+   imports: [ CommonModule ],
+   templateUrl: './toolbar-shadow.component.html',
+   styleUrls: [ './toolbar-shadow.component.scss' ],
 })
 export class ToolbarShadowComponent {
 
