94b4659546b714a5a21461d3108721a7799e6ffe
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-21 20:37:20 +0200

    feat: implement authorized$

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpService } from "../http/http.service";
+import { catchError, map, Observable, of } from "rxjs";
+import { API_PROTECTED_URL } from "../../environments/constants";
 
 @Injectable({
-  providedIn: 'root'
+   providedIn: 'root',
 })
 export class AuthService {
+   private readonly http = inject(HttpService);
 
-  constructor() { }
+   public readonly authorized$: Observable<boolean> =
+      this.http.getRequest$<object>(API_PROTECTED_URL)
+      .pipe(
+         map((response) => {
+            return response.ok;
+         }),
+         catchError((): Observable<boolean> => {
+            return of(false);
+         }),
+      );
 }
