34543b54972b01facce042b82a71509ea10a1bf9
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-22 18:55:41 +0200

    feat: define navigation items component

---
+++ src/app/navigation-items/navigation-items.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navigation-items',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navigation-items.component.html',
+  styleUrls: ['./navigation-items.component.scss']
+})
+export class NavigationItemsComponent {
+
+}
