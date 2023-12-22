44006f6c054ab5a3fdf2ee577213268482800b35
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-22 18:53:29 +0200

    feat: define home action component

---
+++ src/app/home-action/home-action.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+   selector: 'app-home-action',
+   standalone: true,
+   imports: [ CommonModule ],
+   templateUrl: './home-action.component.html',
+   styleUrls: [ './home-action.component.scss' ],
+})
+export class HomeActionComponent {
+
+}
