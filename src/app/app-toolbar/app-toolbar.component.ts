90f923c2fbe00a11debff71c9de7053d49155588
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-22 18:49:07 +0200

    feat: define app toolbar

---
+++ src/app/app-toolbar/app-toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+   selector: 'app-toolbar',
+   standalone: true,
+   imports: [ CommonModule ],
+   templateUrl: './app-toolbar.component.html',
+   styleUrls: [ './app-toolbar.component.scss' ],
+})
+export class AppToolbarComponent {
+
+}
