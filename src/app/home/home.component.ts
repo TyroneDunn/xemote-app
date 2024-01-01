196fc87f548e69b66457e1543811398d9f4ca587
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-01 21:00:33 +0200

    feat: import material toolbar module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppToolbarComponent } from "../app-toolbar/app-toolbar.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @Component({
    selector: 'app-home',
    standalone: true,
-   imports: [ CommonModule, AppToolbarComponent ],
+   imports: [
+      CommonModule,
+      MatToolbarModule,
+      AppToolbarComponent,
+   ],
    templateUrl: './home.component.html',
    styleUrls: [ './home.component.scss' ],
 })
