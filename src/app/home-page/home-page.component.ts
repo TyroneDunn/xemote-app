574408507116d476941e4d2fa12cd02e6136a19d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-07 21:24:07 +0200

    feat: add landing section skeleton

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppToolbarComponent } from "../app-toolbar/app-toolbar.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { LandingSectionComponent } from '../landing-section/landing-section.component';
 
 @Component({
-   selector: 'app-home-page',
-   standalone: true,
-   imports: [
+   selector   : 'app-home-page',
+   standalone : true,
+   imports    : [
       CommonModule,
       MatToolbarModule,
       AppToolbarComponent,
+      LandingSectionComponent,
    ],
    templateUrl: './home-page.component.html',
-   styleUrls: [ './home-page.component.scss' ],
+   styleUrls  : [ './home-page.component.scss' ],
 })
 export class HomePageComponent {
 
---
+++ src/app/landing-section/landing-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landing-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landing-section.component.html',
+  styleUrls: ['./landing-section.component.scss']
+})
+export class LandingSectionComponent {
+
+}
