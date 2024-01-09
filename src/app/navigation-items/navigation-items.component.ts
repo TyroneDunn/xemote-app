c9b077144371f9fe3fb5314d680e6fcd28b24f2b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-09 21:44:46 +0200

    feat: add navigation router links

--- src/app/navigation-items/navigation-items.component.ts
+++ src/app/navigation-items/navigation-items.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-items',
   standalone: true,
-  imports: [CommonModule],
+   imports: [ CommonModule, RouterLink ],
   templateUrl: './navigation-items.component.html',
   styleUrls: ['./navigation-items.component.scss']
 })
