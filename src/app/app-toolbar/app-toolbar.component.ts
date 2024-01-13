b6f0673896f211b5f9af0ba6779aadc6c083f298
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-13 10:35:47 +0200

    refactor: move home related components to .../home/

--- src/app/app-toolbar/app-toolbar.component.ts
+++ src/app/app-toolbar/app-toolbar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { HomeActionComponent } from "../home-action/home-action.component";
 import { NavigationItemsComponent } from "../navigation-items/navigation-items.component";
 import { SocialLinkItemsComponent } from "../social-link-items/social-link-items.component";
+import { HomeActionComponent } from '../home/home-action/home-action.component';
 
 @Component({
    selector   : 'app-toolbar',
--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppToolbarComponent } from "../app-toolbar/app-toolbar.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { LandingSectionComponent } from '../landing-section/landing-section.component';
 import { OfferingsSectionComponent } from '../offerings-section/offerings-section.component';
 import {
    TestimonialsSectionComponent
 } from '../testimonials-section/testimonials-section.component';
-import { FooterSectionComponent } from '../footer-section/footer-section.component';
+import { AppToolbarComponent } from '../../app-toolbar/app-toolbar.component';
+import { FooterSectionComponent } from '../../footer-section/footer-section.component';
 
 @Component({
    selector   : 'app-home-page',
