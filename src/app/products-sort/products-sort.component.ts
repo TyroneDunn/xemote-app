b50005ceb93f28e2a057e4b3897fad776ec42f5e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-15 18:07:06 +0200

    chore: format

--- src/app/products-sort/products-sort.component.ts
+++ src/app/products-sort/products-sort.component.ts
@@ -6,21 +6,27 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
-  selector: 'app-products-sort',
-  standalone: true,
-   imports: [ CommonModule, MatFormFieldModule, MatIconModule, MatOptionModule, MatSelectModule ],
-  templateUrl: './products-sort.component.html',
-  styleUrls: ['./products-sort.component.scss']
+   selector   : 'app-products-sort',
+   standalone : true,
+   imports    : [
+      CommonModule,
+      MatFormFieldModule,
+      MatIconModule,
+      MatOptionModule,
+      MatSelectModule
+   ],
+   templateUrl: './products-sort.component.html',
+   styleUrls  : [ './products-sort.component.scss' ],
 })
 export class ProductsSortComponent {
-   public readonly sortByOptions  = [
+   public readonly sortByOptions = [
       "Name",
       "Price",
-      "Category"
+      "Category",
    ];
 
    public readonly orderByIconValue = {
       "asc" : "vertical_align_bottom",
-      "desc" : "vertical_align_top",
+      "desc": "vertical_align_top",
    };
 }
