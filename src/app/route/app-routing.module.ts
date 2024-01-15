0fc84fc8052958377dc7605bc9b497beef6ecd3b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-15 18:10:55 +0200

    chore: remove unused components

--- src/app/route/app-routing.module.ts
+++ src/app/route/app-routing.module.ts
@@ -2,13 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPage } from "../login-page/login.page";
 import { AboutPage } from "../about-page/about.page";
-import { ProductsPage } from "../products-page/products.page";
-import { ProductPage } from "../product-page/product.page";
-import { OrdersPageComponent } from "../orders/orders-page/orders-page.component";
-import { OrderPageComponent } from "../orders/order-page/order-page.component";
-import { InventoryPage } from "../inventory-page/inventory.page";
 import { NotFoundPage } from "../not-found-page/not-found.page";
-import { AuthGuard } from "../auth/auth-guard.service";
 import { HomePage } from '../home-page/home.page';
 import { StorePage } from '../store-page/store.page';
 
@@ -17,11 +11,6 @@ const routes: Routes = [
    { path: 'about', component: AboutPage },
    { path: 'store', component: StorePage },
    { path: 'login', component: LoginPage },
-   { path: 'products', component: ProductsPage, canActivate: [AuthGuard] },
-   { path: 'products/:id', component: ProductPage, canActivate: [AuthGuard] },
-   { path: 'orders', component: OrdersPageComponent, canActivate: [AuthGuard] },
-   { path: 'orders/:id', component: OrderPageComponent, canActivate: [AuthGuard] },
-   { path: 'inventory', component: InventoryPage, canActivate: [AuthGuard] },
    { path: '**', component: NotFoundPage },
 ];
 
