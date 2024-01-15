3beee9f31a0e7fbfdd0bb6aaa09d28665d36b675
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-15 14:49:30 +0200

    chore: format

--- src/app/http/http.service.ts
+++ src/app/http/http.service.ts
@@ -15,27 +15,19 @@ export class HttpService {
       .set("credentials", "true"),
    };
 
-   public getRequest$<T>(url: string): Observable<HttpResponse<T>> {
-      return this.http.get<T>(url, this.options).pipe(
-         retry(2),
-      );
-   }
+   public getRequest$ = <T>(url: string) : Observable<HttpResponse<T>> =>
+      this.http.get<T>(url, this.options)
+      .pipe(retry(2));
 
-   public postRequest$(url: string, payload: object): Observable<HttpResponse<object>> {
-      return this.http.post(url, payload, this.options).pipe(
-         retry(2),
-      );
-   }
+   public postRequest$ = (url: string, payload: object) : Observable<HttpResponse<object>> =>
+      this.http.post(url, payload, this.options)
+      .pipe(retry(2));
 
-   public deleteRequest$(url: string): Observable<HttpResponse<object>> {
-      return this.http.delete(url, this.options).pipe(
-         retry(2),
-      );
-   }
+   public deleteRequest$ = (url: string) : Observable<HttpResponse<object>> =>
+      this.http.delete(url, this.options)
+      .pipe(retry(2));
 
-   public patchRequest$<T>(url: string, payload: object): Observable<HttpResponse<T>> {
-      return this.http.patch<T>(url, payload, this.options).pipe(
-         retry(2),
-      );
-   }
+   public patchRequest$ = <T>(url: string, payload: object) : Observable<HttpResponse<T>> =>
+      this.http.patch<T>(url, payload, this.options)
+      .pipe(retry(2));
 }
