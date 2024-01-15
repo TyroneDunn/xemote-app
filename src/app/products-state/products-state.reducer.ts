e9960a85abcea24bdff3f3eef5d84a3e806951a4
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-15 14:34:30 +0200

    feat: add products reducer skeleton

---
+++ src/app/products-state/products-state.reducer.ts
@@ -0,0 +1,18 @@
+import { Action, ActionReducer, createReducer } from '@ngrx/store';
+import { ProductsState } from './products-state.type';
+
+const initialState: ProductsState = {
+   collection: [],
+   selectedProduct: null,
+   query: null,
+   filters: [],
+   sort: null,
+   order: null,
+   count: null,
+   index: null,
+   limit: null,
+};
+
+export const productsReducer: ActionReducer<ProductsState, Action> = createReducer(
+   initialState,
+);
