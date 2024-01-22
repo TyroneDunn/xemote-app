7109be2f5c3a4ccbeb6089333fefc6d743d3e98e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-22 11:01:01 +0200

    style: improve hero section layout

--- src/app/hero-section/hero.section.ts
+++ src/app/hero-section/hero.section.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [CommonModule],
+   imports: [ CommonModule, MatButtonModule ],
   templateUrl: './hero.section.html',
   styleUrls: ['./hero.section.scss']
 })
