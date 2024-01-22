ed66544750b1d33f72cde53d2eb8d038a3581aef
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-22 13:18:42 +0200

    style: improve offerings section layout

--- src/app/offerings-section/offerings.section.ts
+++ src/app/offerings-section/offerings.section.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-offerings-section',
   standalone: true,
-   imports: [ CommonModule, MatCardModule ],
+   imports: [ CommonModule, MatCardModule, MatIconModule ],
   templateUrl: './offerings.section.html',
   styleUrls: ['./offerings.section.scss']
 })
