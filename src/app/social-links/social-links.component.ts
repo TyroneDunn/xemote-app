4f13185a68f608e485ddfa51f98e7b96c09eb2fb
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-23 12:52:51 +0200

    style: use social icons

--- src/app/social-links/social-links.component.ts
+++ src/app/social-links/social-links.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { SocialIconComponent } from '../social-icon/social-icon.component';
 
 @Component({
   selector: 'app-social-links',
   standalone: true,
-  imports: [CommonModule],
+   imports: [ CommonModule, MatButtonModule, SocialIconComponent ],
   templateUrl: './social-links.component.html',
   styleUrls: ['./social-links.component.scss']
 })
