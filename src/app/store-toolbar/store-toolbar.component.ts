326ba45e0d70c60ce1eb39277068e46e415dbf45
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-24 17:17:20 +0200

    style: update store toolbar layout

--- src/app/store-toolbar/store-toolbar.component.ts
+++ src/app/store-toolbar/store-toolbar.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from '../search/search.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ProductsSortComponent } from '../products-sort/products-sort.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-store-toolbar',
   standalone: true,
-   imports: [ CommonModule, SearchComponent, MatToolbarModule ],
+   imports: [
+      CommonModule,
+      SearchComponent,
+      MatToolbarModule,
+      ProductsSortComponent,
+      MatButtonModule,
+      MatIconModule,
+   ],
   templateUrl: './store-toolbar.component.html',
   styleUrls: ['./store-toolbar.component.scss']
 })
