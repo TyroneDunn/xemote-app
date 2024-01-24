7836ee8beb6382ee294a2a30f68efe7207cb2002
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-24 12:07:15 +0200

    feat: implement home action functionality and styling

--- src/app/home-action/home-action.component.ts
+++ src/app/home-action/home-action.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
    selector: 'app-home-action',
    standalone: true,
-   imports: [ CommonModule ],
+   imports: [ CommonModule, RouterLink, NgOptimizedImage ],
    templateUrl: './home-action.component.html',
    styleUrls: [ './home-action.component.scss' ],
 })
 export class HomeActionComponent {
+   public readonly xemoteLogo : string = "https://xemoteiot.co.za/wp-content/uploads/2022/01/Xemote_Remote-Sensing-and-Control-white-letters-290x83.png";
 
 }
