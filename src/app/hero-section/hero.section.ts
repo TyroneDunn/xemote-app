064972c551cccc9cc5feb8793318c440a37b40b4
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-24 14:49:22 +0200

    feat: add hero image

--- src/app/hero-section/hero.section.ts
+++ src/app/hero-section/hero.section.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-   imports: [ CommonModule, MatButtonModule, RouterLink ],
+   imports: [ CommonModule, MatButtonModule, RouterLink, NgOptimizedImage ],
   templateUrl: './hero.section.html',
   styleUrls: ['./hero.section.scss']
 })
