0f609ea2c47a6bd271abddae7d20f6edfc8884a5
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-28 22:03:29 +0200

    style: improve layout

--- src/app/products-api/products-api.service.ts
+++ src/app/products-api/products-api.service.ts
@@ -1,8 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpService } from '../http/http.service';
 import { map, Observable } from 'rxjs';
-import { ProductsRequest, GetProductsResponse } from '../product/product.types';
+import { GetProductsResponse, ProductsRequest } from '../products/product.types';
 import { API_PRODUCTS_URL } from '../../environments/constants';
+import { HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,41 +11,36 @@ import { API_PRODUCTS_URL } from '../../environments/constants';
 export class ProductsApiService {
    private http: HttpService = inject(HttpService);
 
-   public getProducts$ = (getProductsRequest: ProductsRequest): Observable<GetProductsResponse> => {
-      const getProductsUrl: string = this.mapGetProductsRequestToGetProductsUrl(getProductsRequest);
-      return this.http.getRequest$<GetProductsResponse>(getProductsUrl)
-         .pipe(
-            map((response) => {
-               console.log('http response: ', response.body);
-               return (response.body as GetProductsResponse);
-            })
-         );
-   };
-
-   private mapGetProductsRequestToGetProductsUrl = (getProductsRequest : ProductsRequest) : string => {
-      const url : URL = new URL(API_PRODUCTS_URL);
-      if (getProductsRequest.filter?.nameRegex)
-         url.searchParams.append('nameRegex', getProductsRequest.filter.nameRegex);
-      if (
-         getProductsRequest.filter?.byCategories
-         && getProductsRequest.filter.byCategories.filter(value => value.active).length > 0
-      ) {
-         url.searchParams.append('category', JSON.stringify(
-            getProductsRequest.filter.byCategories
-            .filter(categoryFilter => categoryFilter.active)
-            .map(categoryFilter => categoryFilter.category)
-         ));
-      }
-      if (getProductsRequest.filter?.priceRange)
-         url.searchParams.append('price', JSON.stringify(getProductsRequest.filter.priceRange));
-      if (getProductsRequest.sort) {
-         url.searchParams.append('sort', getProductsRequest.sort.field);
-         url.searchParams.append('order', getProductsRequest.sort.order);
-      }
-      if (getProductsRequest.page) {
-         url.searchParams.append('index', getProductsRequest.page.index.toString());
-         url.searchParams.append('limit', getProductsRequest.page.limit.toString());
-      }
-      return url.toString();
-   };
+   public getProducts$ = (getProductsRequest: ProductsRequest): Observable<GetProductsResponse> =>
+      this.http.getRequest$<GetProductsResponse>(mapGetProductsRequestToGetProductsUrl(getProductsRequest))
+      .pipe(
+         map((response : HttpResponse<GetProductsResponse>) =>
+            (response.body as GetProductsResponse)));
 }
+
+const mapGetProductsRequestToGetProductsUrl = (getProductsRequest : ProductsRequest) : string => {
+   const url : URL = new URL(API_PRODUCTS_URL);
+   if (getProductsRequest.filter?.nameRegex)
+      url.searchParams.append('nameRegex', getProductsRequest.filter.nameRegex);
+   if (
+      getProductsRequest.filter?.byCategories
+      && getProductsRequest.filter.byCategories.filter(value => value.active).length > 0
+   ) {
+      url.searchParams.append('category', JSON.stringify(
+         getProductsRequest.filter.byCategories
+         .filter(categoryFilter => categoryFilter.active)
+         .map(categoryFilter => categoryFilter.category)
+      ));
+   }
+   if (getProductsRequest.filter?.priceRange)
+      url.searchParams.append('price', JSON.stringify(getProductsRequest.filter.priceRange));
+   if (getProductsRequest.sort) {
+      url.searchParams.append('sort', getProductsRequest.sort.field);
+      url.searchParams.append('order', getProductsRequest.sort.order);
+   }
+   if (getProductsRequest.page) {
+      url.searchParams.append('index', getProductsRequest.page.index.toString());
+      url.searchParams.append('limit', getProductsRequest.page.limit.toString());
+   }
+   return url.toString();
+};
