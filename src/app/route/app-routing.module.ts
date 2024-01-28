cbaee687e0817347f037f078b90557ec73388bd5
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-28 21:20:26 +0200

    refactor: remove unused routes

--- src/app/route/app-routing.module.ts
+++ src/app/route/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPage } from "../login-page/login.page";
-import { AboutPage } from "../about-page/about.page";
-import { NotFoundPage } from "../not-found-page/not-found.page";
 import { HomePage } from '../home-page/home.page';
 import { StorePage } from '../store-page/store.page';
+import { NotFoundPage } from "../not-found-page/not-found.page";
 
 const routes: Routes = [
    { path: '', component: HomePage },
-   { path: 'about', component: AboutPage },
    { path: 'store', component: StorePage },
-   { path: 'login', component: LoginPage },
    { path: '**', component: NotFoundPage },
 ];
 
